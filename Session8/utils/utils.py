450f06744b65f022a25b3a42744c1c774fecfb13
Harsha Vardhan M A <harshavardhan.ma@gmail.com>
2020-03-11 01:16:34 +0530

    Add files via upload

---
+++ Session8/utils/utils.py
@@ -0,0 +1,7 @@
+from torchsummary import summary
+
+def get_summary(model, input):
+	use_cuda = torch.cuda.is_available()
+	device = torch.device("cuda" if use_cuda else "cpu")
+	model = model.to(device)
+	return summary(model, input_size=(3,32,32))
